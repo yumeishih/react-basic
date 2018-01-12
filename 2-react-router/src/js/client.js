8eb4631f8d2a17f839f5d45256808f5afede70ba
yumi <yumeishih17@gmail.com>
2018-01-12 10:19:01 +0800

    refacter code

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -1,22 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute,hashHistory } from "react-router";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Layout from "./pages/Layout";
-import Featured from "./pages/Featured";
 import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
+import Layout from "./pages/Layout";
 import Settings from "./pages/Settings";
 
-
-console.log({hashHistory});
 const app = document.getElementById('app');
-ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Layout}>
-            <IndexRoute component={Featured}></IndexRoute>
-            <Route path="archives(/:article)" component={Archives}></Route>
-            <Route path="settings" component={Settings}></Route>
 
-        </Route>
-    </Router>,
-app); // url with parentheses means optional
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+  </Router>,
+app);
--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,37 +1,34 @@
 import React from "react";
 import { Link } from "react-router";
 
-import Footer from "../compomnents/layout/Footer";
-import Nav from "../compomnents/layout/Nav";
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
 
 export default class Layout extends React.Component {
-    navigate() {
-        this.props.router.push("/");
-        /* Info
-            this.props.history.pushState() and .replaceState() functions have been deprecated by react-router
-        */
-    }
-  render() {
-    const { router } =this.props;
-    const { location } = this.props;
-    const containerStyle = {
-        marginTop: "60px"
-    };
-    console.log("layout");
-    console.log(router.isActive("archives"));
-    return (
-        <div>
+    render() {
+        const { location } = this.props;
+        const containerStyle = {
+          marginTop: "60px"
+        };
+        console.log("layout");
+        return (
+          <div>
+
             <Nav location={location} />
+
             <div class="container" style={containerStyle}>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1>KillerNews.net</h1>
-                        {this.props.children}
-                    </div>
+              <div class="row">
+                <div class="col-lg-12">
+                  <h1>KillerNews.net</h1>
+
+                  {this.props.children}
+
                 </div>
-                <Footer />
+              </div>
+              <Footer/>
             </div>
-        </div>
-    );
-  }
+          </div>
+
+        );
+      }
 }
